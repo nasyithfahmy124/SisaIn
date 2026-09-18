a1535f4a2d7f72fa819e8e6380bd1a2f49d49bac
Nasyith Fahmy <nasyithfahmy124@gmail.com>
2026-09-18 23:51:05 +0700

    log google

--- core/account/models.py
+++ core/account/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+User = get_user_model()
 # Create your models here.
+class AkunProfile(models.Model):
+    nama = models.ForeignKey(User,on_delete=models.CASCADE)
+    email = models.ForeignKey(User,on_delete=models.CASCADE)
+    
